684fc6d9a4c91936f4614c824d043926c0d3e3d3
Ian Chamberlain <ichamberlain@juniper.net>
2022-08-16 17:11:11 -0400

    Initial commit

---
+++ testdata/missing_trailing.go
@@ -0,0 +1,36 @@
+package foo
+
+type Struct struct {
+	i, j int
+	k bool
+}
+
+func main() {
+	x := []string{
+		"1",
+		"2",
+		"3"
+	}
+
+	bar := Struct{
+		1,
+		2,
+		false
+	}
+
+	baz := Struct{
+		1,
+		2,
+		false
+	}
+}
+
+func Foo(
+	int i
+) {}
+
+func(Struct) Bar(
+	int j
+) {}
+
+
